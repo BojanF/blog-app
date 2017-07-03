caba01bfbc447280b647114668586e0166d795f6
Jovica <jovicaivanoski@gmail.com>
2017-07-03 21:43:08 +0200

    New Atributes to User entity

--- src/BlogApp/Models/Users.cs
+++ src/BlogApp/Models/Users.cs
@@ -8,6 +8,9 @@ using BlogApp.Models.enums;
 namespace BlogApp.Models {
     public class Users {
         public int ID { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+
         public UserRole role;
 
     }// end User class  .
