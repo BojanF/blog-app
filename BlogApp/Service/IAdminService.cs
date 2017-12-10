6f28f7ed808e3428bdb81d0bfcffb9ec26b64066
BojanF <boko1994@gmail.com>
2017-12-10 16:15:56 +0100

    New NuGet packages added as dependencies in BlogApp.csproj

--- BlogApp/Service/IAdminService.cs
+++ BlogApp/Service/IAdminService.cs
@@ -6,10 +6,10 @@ using BlogApp.Models;
 
 
 namespace BlogApp.Services {
-    interface AdminService {
+    interface IAdminService {
         void ApprovePost(long PostId, long AdminId, long UserId);
-        Notifications DisapprovePost(long PostId, long AdminId, long UserId, string Text);
-        Categories AddCategory(long CategoryId, string CategoryName, int LikesRule, int CommentsRule, int PostRule);
+        Notification DisapprovePost(long PostId, long AdminId, long UserId, string Text);
+        Category AddCategory(long CategoryId, string CategoryName, int LikesRule, int CommentsRule, int PostRule);
         void DeleteComment(long CommentId, long AdminId, long UserId);
 
     }
--- BlogApp/Service/IUserService.cs
+++ BlogApp/Service/IUserService.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 using BlogApp.Models;
 
 namespace BlogApp.Services {
-    interface UserService {
-        Posts BlogPost(string UserId, long CategoryId, string Caption, string Headline, string PostedText);
+    interface IUserService {
+        Post BlogPost(string UserId, long CategoryId, string Caption, string Headline, string PostedText);
         void EditPost(string UserId, long PostId, string Caption, string Headline, string PostedText);
-        Comments CommentOnPost(long PostId, long UserId, string CommentText);
+        Comment CommentOnPost(long PostId, long UserId, string CommentText);
         void EditOwnComment(long CommentId, string CommentText);
         void DeleteOwnComment(long PostId, long CommentId);
         void DeleteCommentOnOwnPost(long PostId, long CommentId);
         bool Like(long PostId);
-        Notifications Report(string UserId, long ReportedUserId, string Text);
+        Notification Report(string UserId, long ReportedUserId, string Text);
     }
 }
